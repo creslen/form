dd96fbeabe98d785ba499f14729c709e6ff4c276
creslen <boncalescreslenjoy@gmail.com>
2023-05-09 21:25:07 +0800

    smthng

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const insert = () => {
                     break
         case 'radio': appendRadio(label, placeholder)
                     break
+        case 'button': appendButton(label, placeholder)
+                    break
 
 
     }
@@ -19,6 +21,7 @@ const appendText = (label = "No label", placeholder = "No placeholder") => {
     let del = document.createElement('button')
     del.setAttribute('onclick','document.getElementById("'+id+'").style.display="none"')
     del.textContent="delete"
+    del.setAttribute('class','btn btn-primary')
 
     let el = document.createElement('input')
     el.setAttribute('class', 'form-control mt-1')
@@ -40,18 +43,41 @@ const appendRadio = (label = "No label", placeholder = "No placeholder") => {
     let del = document.createElement('button')
     del.setAttribute('onclick','document.getElementById("'+id+'").style.display="none"')
     del.textContent="delete"
+    del.setAttribute('class','btn btn-primary')
 
     let el = document.createElement('input')
     el.setAttribute('class', ' mt-1')
     el.setAttribute('placeholder', placeholder)
     el.setAttribute('type','radio')
     let lbl=document.createElement('label')
-    lbl.textContent =label,"you have inserted radio"
+    lbl.textContent =label
     lbl.textContent ="you have inserted radio"
     lbl.setAttribute('class','mx-1 mt-1')
+  
+    wrap.append(el,lbl,del)
+    document.querySelector("#elements").append(wrap) 
+    id++
+}
+const appendButton = (label = "No label", placeholder = "No placeholder") => {
+
+   let wrap = document.createElement('div')
+    wrap.setAttribute('id',id)
+    let del = document.createElement('button')
+    del.setAttribute('onclick','document.getElementById("'+id+'").style.display="none"')
+    del.textContent="delete"
+    del.setAttribute('class','btn btn-primary')
+
+    let el = document.createElement('button')
+    el.setAttribute('class', 'btn btn-primary')
+    el.setAttribute('placeholder', placeholder)
+    el.setAttribute('type','button')
+    el.textContent = label;
+
+    let lbl = document.createElement('label');
+    lbl.textContent = "You have inserted a button->";
+  
   
     wrap.append(lbl,el,del)
     document.querySelector("#elements").append(wrap) 
     id++
 }
-
